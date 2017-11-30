eeced9ff8778d17085cd85a5faeb05e9a5840f19
alannaarabian <31958747+alannaarabian@users.noreply.github.com>
2017-11-30 14:44:40 -0500

    workinghtml

--- mycharacterbio.py
+++ mycharacterbio.py
@@ -2,17 +2,17 @@
 
 # These are my questions to be asked in the survey
 
-q1 = 'What is your name? '
-q2 = 'Where do you live? '
-q3 = 'What is your job? '
-q4 = 'What is your biggest fear?'
-q5 = 'what is your biggest regret?'
-q6 = 'who are the people in your life that you love the most?'
-q7 = 'what is a pet peeve of yours?'
-q8 = 'what do you do for fun?'
-q9= 'what is something you are really good at doing?'
-q10 = 'what time of day is it?'
-q11 = 'what is the weather like?'
+q1 = 'What is your name? \n' 
+q2 = 'Where do you live? \n'
+q3 = 'What is your job? \n'
+q4 = 'What is your biggest fear? \n'
+q5 = 'what is your biggest regret? \n'
+q6 = 'who are the people in your life that you love the most? \n'
+q7 = 'what is a pet peeve of yours? \n'
+q8 = 'what do you do for fun? \n'
+q9= 'what is something you are really good at doing? \n'
+q10 = 'what time of day is it? \n'
+q11 = 'what is the weather like? \n'
 
 
 
@@ -38,44 +38,47 @@ r11= input(q11)
 
 # open and write to a file (character_bio.txt)
 
-file_name = 'character_bio.text'
+file_name = 'character_bio.html'
 
 f = open(file_name, 'w') # open a file and plan to write on it
 
 # write the questions and resposnes on the file
+f.write("<html>")
 
-f.write(q1 + '\n')
-f.write(r1 + '\n\n')
+f.write('<p style = "color: blue">' + q1 + '</p>' )
+f.write(r1 + '<br><br>')
 
-f.write(q2 + '\n')
-f.write(r2 + '\n\n')
+f.write(q2 + '<br>')
+f.write(r2 + '<br><br>')
 
-f.write(q3 + '\n')
-f.write(r3 + '\n\n')
+f.write(q3 + '<br>')
+f.write(r3 + '<br><br>')
 
-f.write(q4 + '\n')
-f.write(r4 + '\n\n')
+f.write(q4 + '<br>')
+f.write(r4 + '<br><br>')
 
-f.write(q5 + '\n')
-f.write(r5 + '\n\n')
+f.write(q5 + '<br>')
+f.write(r5 + '<br><br>')
 
-f.write(q6 + '\n')
-f.write(r6 + '\n\n')
+f.write(q6 + '<br>')
+f.write(r6 + '<br><br>')
 
-f.write(q7 + '\n')
-f.write(r7 + '\n\n')
+f.write(q7 + '<br>')
+f.write(r7 + '<br><br>')
 
-f.write(q8 + '\n')
-f.write(r8 + '\n\n')
+f.write(q8 + '<br>')
+f.write(r8 + '<br><br>')
 
-f.write(q9 + '\n')
-f.write(r9 + '\n\n')
+f.write(q9 + '<br>')
+f.write(r9 + '<br><br>')
 
-f.write(q10 + '\n')
-f.write(r10 + '\n\n')
+f.write(q10 + '<br>')
+f.write(r10 + '<br><br>')
 
-f.write(q11 + '\n')
-f.write(q11 + '\n\n')
+f.write(q11 + '<br>')
+f.write(q11 + '<br><br>')
+
+f.write("</html>")
         
 
 
