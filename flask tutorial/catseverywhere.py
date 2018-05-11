ec6c38f4216a73c0652395829d905e6bb104dfbc
alannaarabian <31958747+alannaarabian@users.noreply.github.com>
2018-05-11 14:53:06 -0400

    updated

--- flask tutorial/catseverywhere.py
+++ flask tutorial/catseverywhere.py
@@ -1,5 +1,5 @@
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 app = Flask(__name__)
 
 @app.route('/')
@@ -8,6 +8,11 @@ def hello_world():
    name = "alanna"
    return render_template('index.html', author=author, name=name)
 
+@app.route('/signup', methods = ['POST'])
+def signup():
+    email = request.form['email']
+    print("The email address is '" + email + "'")
+    return redirect('/')
 
 if __name__ == "__main__":
     app.run()
